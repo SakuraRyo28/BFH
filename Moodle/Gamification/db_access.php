a4b5721c11e93f7ec9b51a990045974c3e27ed6f
SakuraRyo28 <159829301+SakuraRyo28@users.noreply.github.com>
2025-12-03 13:33:32 +0100

    Create db_access.php

---
+++ Moodle/Gamification/db_access.php
@@ -0,0 +1,33 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$capabilities = [
+    'local/quest:view' => [
+        'captype' => 'read',
+        'contextlevel' => CONTEXT_COURSE,
+        'archetypes' => [
+            'student' => CAP_ALLOW,
+            'teacher' => CAP_ALLOW,
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW,
+        ],
+    ],
+    'local/quest:manage' => [
+        'riskbitmask' => RISK_SPAM | RISK_XSS,
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_COURSE,
+        'archetypes' => [
+            'teacher' => CAP_ALLOW,
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW,
+        ],
+    ],
+    'local/quest:earnpoints' => [
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_COURSE,
+        'archetypes' => [
+            'student' => CAP_ALLOW,
+        ],
+    ],
+];
+?>
